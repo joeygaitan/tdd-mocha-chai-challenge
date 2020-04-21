4949313892ea41c5ac3938ef50ab98dc65cbc988
joeygaitan <joeygaitan707@gmail.com>
2020-04-21 04:23:47 -0700

    setting up intial repo

---
+++ tests/utils.test.js
@@ -0,0 +1,110 @@
+const mocha = require("mocha")
+const chai = require("chai")
+const utils = require("../utils")
+const expect = chai.expect
+
+//added in chai-things for short hand of properties :)
+chai.should();
+chai.use(require('chai-things'));
+
+// ========================================================
+// NOTE: https://mochajs.org/#arrow-functions
+// Passing arrow functions (“lambdas”) to Mocha is discouraged.
+// Lambdas lexically bind this and cannot access the Mocha context.
+// ========================================================
+
+it("should say hello", function() {
+  const hello = utils.sayHello()
+  expect(hello).to.be.a("string")
+  expect(hello).to.equal("Hello")
+  expect(hello).with.lengthOf(5)
+})
+
+// ========================================================
+// Level 1 Challenges
+// ========================================================
+
+it("should return the area of a 5 by 6 rectangle", function (){
+  const area = utils.area(5,6)
+  expect(area).to.be.a("number")
+  expect(area).to.be.equal(30)
+  
+  const areaNegative = utils.area(-2, -4)
+  expect(areaNegative).to.be.equal(false)
+})
+
+it("should return the perimeter of a 5 by 6 rectangle", function(){
+  const perimeter = utils.perimeter(5,6)
+  expect(perimeter).to.be.a("number")
+  expect(perimeter).to.be.equal(22)
+
+  const negativePerimeter = utils.perimeter(4,-6)
+  expect(negativePerimeter).to.be.equal(false)
+})
+
+it("should return the are of a circle of radius 5", function () {
+  const circleArea = utils.circleArea(5)
+  expect(circleArea).to.be.a("number")
+  expect(circleArea).to.be.equal(78.53981633974483)
+})
+
+// ========================================================
+// Level 2 Challenges
+// ========================================================
+// NOTE: The following unimplemented test cases are examples
+// of "Pending Tests" in Chai. Someone should write these
+// tests eventually.
+// ========================================================
+
+it("Should create a new (object) Item with name and price", function () {
+  const inventoryItem = utils.createItem('apple',0.99)
+
+  expect(inventoryItem).to.be.a('object')
+  expect(inventoryItem.name).to.be.a('string')
+  expect(inventoryItem.price).to.be.a('number')
+  expect(inventoryItem.quantity).to.be.a('number')
+  
+  expect(inventoryItem.name).to.be.equal('apple')
+  expect(inventoryItem.price).to.be.equal(0.99)
+  expect(inventoryItem.quantity).to.be.equal(1)
+
+  //chai things == properties. Checks all properties are inside basically property
+  
+})
+
+it("Should return an array containing all items in cart", function(){
+  utils.addItemToShoppingCart('apple',0.99)
+  utils.addItemToShoppingCart('peach',2.00,3)
+  utils.addItemToShoppingCart('orange',1.50,8)
+
+  expect(utils.shoppingCart.length).to.be.equal(3)
+
+  utils.clearShoppingCart()
+})
+
+it("Should add a new item to the shopping cart", function () {
+  const cartItem1 = utils.addItemToShoppingCart('apple',0.99)
+  const cartItem2 = utils.addItemToShoppingCart('peach',2.00,3)
+  const cartItem3 = utils.addItemToShoppingCart('orange',1.50,8)
+
+  utils.shoppingCart.should.include.something.that.deep.equals(cartItem1)
+  utils.shoppingCart.should.include.something.that.deep.equals(cartItem2)
+  utils.shoppingCart.should.include.something.that.deep.equals(cartItem3)
+
+
+  utils.clearShoppingCart()
+})
+
+it("Should return the number of items in the cart")
+
+it("Should remove items from cart")
+
+// ========================================================
+// Stretch Challenges
+// ========================================================
+
+it("Should update the count of items in the cart")
+
+it("Should validate that an empty cart has 0 items")
+
+it("Should return the total cost of all items in the cart")
---
+++ utils.js
@@ -0,0 +1,132 @@
+// ========================================================
+// Level 1 Challenges
+// ========================================================
+
+const sayHello = () => {
+  return "Hello"
+}
+
+const area = (w, h) => {
+  if (w >= 1 && h >= 1) {
+    return w * h
+  } else {
+    return false
+  }
+}
+
+const perimeter = (w, h) => {
+  if (w >= 1 && h >= 1) {
+    return w + w + h + h
+  } else {
+    return false
+  }
+}
+
+const circleArea = r => {
+  return Math.PI * r * r
+}
+
+// ========================================================
+// Level 2 Challenges
+// ========================================================
+// NOTE: You will need to implement methods below (not yet
+// defined) in order to make the tests pass.
+// ========================================================
+
+let shoppingCart = []
+let storeInventory = []
+
+const createItem = (name,price, quantity=1) => {
+  
+  //should return a JSON object for the item
+
+  storeInventory.push({name, price, quantity})
+
+  console.log(storeInventory)
+
+  return {name, price, quantity}
+}
+
+
+
+const addItemToShoppingCart = item => {
+  //should add it to shopping cart
+  
+  for (let i = 0;i < shoppingCart.length; i += 1) {
+    if (shoppingCart[i].name == item.name) {
+      shoppingCart[i].price = item.price
+      shoppingCart[i].quantity += item.quantity
+      return shoppingCart[i]
+    }
+  }
+  shoppingCart.push(createItem(item))
+  return shoppingCart
+}
+
+const displayShoppingCart = () => {
+  if (shoppingCart.length == 0){
+    return 'empty shopping cart please add items to shopping cart'
+  } else {
+    return shoppingCart
+  }
+}
+
+const displayStoreInventory = () => {
+  if (storeInventory.length == 0) {
+    return 'no items inventory please contact administration for error or lack of shipments'
+  } else {
+    return storeInventory
+  }
+}
+
+const numberOfItemsInCart = () => {
+  if (shoppingCart.length == 0) {
+    return 'empty shopping cart please add items to shopping cart'
+  } else {
+    let counter = 0
+    for (let i = 0; i < shoppingCart.length;i += 1) {
+      counter += shoppingCart[i].quantity
+    }
+    return counter
+  }
+}
+
+const remoteItemFromCart = item => {
+  if (shoppingCart.length == 0) {
+    return 'empty shopping cart please add items to shopping cart'
+  } else {
+    for (let i = 0; i < shoppingCart.length; i += 1) {
+      if (shoppingCart.name == item.name) {
+        shoppingCart.splice(i, 1)
+      }
+    }
+    return shoppingCart
+  }
+}
+
+const totalCostOfCart = () => {
+  if (shoppingCart.length == 0) {
+    return 'empty shopping cart please add items to shopping cart'
+  } else {
+    let amount = 0
+    for (let i = 0; i < shoppingCart.length; i += 1) {
+      amount += shoppingCart[i].price * shoppingCart[i].quantity
+      amount
+    }
+    amount = amount.toFixed(2)
+    return amount
+  }
+}
+
+const clearShoppingCart = () => {
+  if (shoppingCart.length == 0) {
+    return 'empty shopping cart please add items to shopping cart'
+  } else {
+    shoppingCart.splice(0,shoppingCart.length)
+    return 'cart empty'
+  }
+}
+
+module.exports = { 
+  sayHello, area, perimeter, circleArea, createItem, addItemToShoppingCart, shoppingCart, storeInventory, displayShoppingCart, displayStoreInventory, numberOfItemsInCart, remoteItemFromCart, totalCostOfCart, clearShoppingCart
+}
